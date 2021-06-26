025546b86ffd1e23a07188593f9269dfea9ba7ab
Zhenzhen Zhao <me@triplez.cn>
2021-06-26 13:47:41 +0800

    feat: add transactions' metadata for Alipay/WeChat bills (#25)
    * feat(alipay): add tx metadata
    
    Signed-off-by: Triple-Z <me@triplez.cn>
    
    * feat(wechat): add tx metadata
    
    Signed-off-by: Triple-Z <me@triplez.cn>

--- pkg/provider/wechat/convert.go
+++ pkg/provider/wechat/convert.go
@@ -18,6 +18,7 @@ func (w *Wechat) convertToIR() *ir.IR {
 			TypeOriginal: o.TypeOriginal,
 			Method:       o.Method,
 		}
+		irO.Metadata = getMetadata(o)
 		if o.MechantOrderID != "" {
 			irO.MerchantOrderID = &o.MechantOrderID
 		}
@@ -34,5 +35,44 @@ func conevertType(t OrderType) ir.TxType {
 		return ir.TxTypeRecv
 	default:
 		return ir.TxTypeUnknown
+	}
+}
+
+func getMetadata(o Order) map[string]string {
+	// FIXME(TripleZ): hard-coded, bad pattern
+	source := "微信支付"
+	var status, method, category, txType, orderId, merchantId, paytime string
+
+	paytime = o.PayTime.String()
+
+	if o.OrderID != "" {
+		orderId = o.OrderID
+	}
+
+	if o.MechantOrderID != "" {
+		merchantId = o.MechantOrderID
+	}
+
+	if o.TypeOriginal != "" {
+		txType = o.TypeOriginal
+	}
+
+	if o.Method != "" {
+		method = o.Method
+	}
+
+	if o.Status != "" {
+		status = o.Status
+	}
+
+	return map[string]string{
+		"source":     source,
+		"payTime":    paytime,
+		"orderId":    orderId,
+		"merchantId": merchantId,
+		"txType":     txType,
+		"category":   category,
+		"method":     method,
+		"status":     status,
 	}
 }
