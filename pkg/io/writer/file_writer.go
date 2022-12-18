5b424f7ccd4ae815fe19b3c439688c607050bce3
TripleZ <me@triplez.cn>
2022-12-18 16:50:10 +0800

    fix: build error in normal mode
    Signed-off-by: TripleZ <me@triplez.cn>

--- pkg/io/writer/file_writer.go
+++ pkg/io/writer/file_writer.go
@@ -5,11 +5,12 @@ package writer
 
 import (
 	"fmt"
+	"log"
 	"os"
 )
 
 func GetWriter(outputFile string) (OutputWriter, error) {
-	log.Printf("Writing to %s", b.Output)
+	log.Printf("Writing to %s", outputFile)
 	file, err := os.Create(outputFile)
 	if err != nil {
 		return nil, fmt.Errorf("create output file  %s error: %v", outputFile, err)
