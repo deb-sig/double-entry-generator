b6fc91127bc67f540387e003f0f158e3dc8705b8
Ramsay <ramsayleung@gmail.com>
2023-05-19 18:46:53 -0700

    增加wechat 测试

---
+++ test/wechat-test-ledger.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+#
+# E2E test for wechat provider.
+
+# set -x # debug
+set -eo errexit
+
+TEST_DIR=$(dirname "$(realpath $0)")
+ROOT_DIR="$TEST_DIR/.."
+OUTPUT="$ROOT_DIR/test/output/test-wechat-output.ledger"
+
+make -f "$ROOT_DIR/Makefile" build
+mkdir -p "$ROOT_DIR/test/output"
+
+# generate wechat bills output in beancount format
+"$ROOT_DIR/bin/double-entry-generator" translate \
+    --provider wechat \
+    --target ledger \
+    --config "$ROOT_DIR/example/wechat/config.yaml" \
+    --output "$OUTPUT" \
+    "$ROOT_DIR/example/wechat/example-wechat-records.csv"
+
+diff -u --color \
+    "$ROOT_DIR/example/wechat/example-wechat-output.ledger" \
+    "$OUTPUT"
+
+if [ $? -ne 0 ]; then
+    echo "[FAIL] WeChat provider output is different from expected output."
+    exit 1
+fi
+
+echo "[PASS] All WeChat provider for ledger target tests!"
