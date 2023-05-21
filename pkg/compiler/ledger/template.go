7898b48c050a41d2322b117b6dadbba75b65fe75
Ramsay <ramsayleung@gmail.com>
2023-05-20 23:18:34 -0700

    增加火币账户支持

--- pkg/compiler/ledger/template.go
+++ pkg/compiler/ledger/template.go
@@ -42,14 +42,105 @@ type NormalOrderVars struct {
 	Currency          string            // 货币
 }
 
-var (
-	normalOrderTemplate *template.Template
-)
+// 火币买入模版（手续费单位为购买单位货币）
 
-// 火币买入模板(TODO)
+/**
+ledger 支持单价 * 数量, 如
+; cost per item
+2010/05/31 * Market
+  Assets:Fridge                35 apples @ $0.42
+  Assets:Cash
 
-// 火币买入模板2(TODO)
+或者总价与数量(自动算出单价)
+; total cost
+2010/05/31 * Market
+  Assets:Fridge                35 apples @@ $14.70
+  Assets:Cash
 
-// 火币卖出模版(TODO)
+但不能像beancount 那样子支持同时指定单价与总价.
+2021-02-23 * "Huobi-币币交易" "买入-BTC1S/USDT"
+	Assets:Rule1:Positions     4.57600000 "BTC1S" {1.22520000 "USDT" } @@ 5.60652159 "USDT"
+	Assets:Cash
+
+因为浮点数精度的原因, 4.57600000(数量) * 1.22520000(单价) = 5.6065152000000000, 而非 5.60652159, 就会导致对账不平
+
+因此以总价及数量为标, 单价作为注释参考
+2021-02-23 * "Huobi-币币交易" "买入-BTC1S/USDT"
+	Assets:Rule1:Positions     4.57600000 "BTC1S" @@ 5.60652159 "USDT"; {1.22520000 "USDT" }
+	Assets:Cash
+**/
+
+// 火币的货币中可能包含数字, 如BTC1S, ledger 包含数字的货币解析成金额，然后报错，因此需要使用双引号 "BTC1S"
+var huobiTradeBuyOrder = `{{ .PayTime.Format "2006-01-02" }} * "{{ .Peer }}-{{ .TxTypeOriginal }}" "{{ .TypeOriginal }}-{{ .Item }}"
+    {{ .CashAccount }}     -{{ .Money | printf "%.8f" }} "{{ .BaseUnit }}"
+    {{ .PositionAccount }}     {{ .Amount | printf "%.8f" }} "{{ .TargetUnit }}" @@ {{ .Money | printf "%.8f" }} "{{ .BaseUnit }}" ; { {{- .Price | printf "%.8f" }} "{{ .BaseUnit -}}" } 
+    {{ .CashAccount }}     -{{ .Commission | printf "%.8f" }} "{{ .TargetUnit }}" @ {{ .Price | printf "%.8f" }} "{{ .BaseUnit }}"
+    {{ .CommissionAccount }}     {{ .Commission | printf "%.8f" }} "{{ .CommissionUnit }}" @ {{ .Price | printf "%.8f" }} "{{ .BaseUnit }}"
+
+`
+
+type HuobiTradeBuyOrderVars struct {
+	PayTime           time.Time // 交易时间
+	Peer              string    // 交易对手
+	TxTypeOriginal    string    // 交易类型(币币交易)
+	TypeOriginal      string    // 操作类型(买入/卖出)
+	Item              string    // 交易商品
+	CashAccount       string    // 现金账号
+	PositionAccount   string
+	CommissionAccount string // 手续费账号
+	PnlAccount        string
+	Amount            float64 // 数量
+	Money             float64 // 金额
+	Commission        float64 // 手续费
+	Price             float64 // 单价
+	BaseUnit          string  // 支出货币类型
+	TargetUnit        string  // 目标货币类型
+	CommissionUnit    string  // 手续费货币类型
+}
+
+// 火币买入模版 2（手续费为特定货币）
+var huobiTradeBuyOrderDiffCommissionUnit = `{{ .PayTime.Format "2006-01-02" }} * "{{ .Peer }}-{{ .TxTypeOriginal }}" "{{ .TypeOriginal }}-{{ .Item }}"
+    {{ .CashAccount }}     -{{ .Money | printf "%.8f" }} "{{ .BaseUnit }}"
+    {{ .PositionAccount }}     {{ .Amount | printf "%.8f" }} "{{ .TargetUnit }}" @@ {{ .Money | printf "%.8f" }} "{{ .BaseUnit }}"; { {{- .Price | printf "%.4f" }} {{ .BaseUnit -}} }
+    {{ .PositionAccount }}     -{{ .Commission | printf "%.8f" }} "{{ .CommissionUnit }}"
+    {{ .CommissionAccount }}     {{ .Commission | printf "%.8f" }} "{{ .CommissionUnit }}"
+
+`
+
+// 火币卖出模版
+var huobiTradeSellOrder = `{{ .PayTime.Format "2006-01-02" }} * "{{ .Peer }}-{{ .TxTypeOriginal }}" "{{ .TypeOriginal }}-{{ .Item }}"
+    {{ .PositionAccount }}     -{{ .Amount | printf "%.8f" }} "{{ .TargetUnit }}" @ {{ .Price | printf "%.8f" }} "{{ .BaseUnit }}"
+    {{ .CashAccount }}     {{ .Money | printf "%.8f" }} "{{ .BaseUnit }}"
+    {{ .CashAccount }}     -{{ .Commission | printf "%.8f" }} "{{ .CommissionUnit }}"
+    {{ .CommissionAccount }}     {{ .Commission | printf "%.8f" }} "{{ .CommissionUnit }}"
+    {{ .PnlAccount }}
+
+`
+
+type HuobiTradeSellOrderVars struct {
+	PayTime           time.Time // 交易时间
+	Peer              string    // 交易对手
+	TxTypeOriginal    string    // 交易类型(币币交易)
+	TypeOriginal      string    // 操作类型(买入/卖出)
+	Item              string    // 交易商品
+	CashAccount       string    // 现金账号
+	PositionAccount   string
+	CommissionAccount string // 手续费账号
+	PnlAccount        string
+	Amount            float64 // 数量
+	Money             float64 // 金额
+	Commission        float64 // 手续费
+	Price             float64 // 单价
+	BaseUnit          string  // 支出货币类型
+	TargetUnit        string  // 目标货币类型
+	CommissionUnit    string  // 手续费货币类型
+}
 
 // 海通买入模版(TODO)
+
+var (
+	normalOrderTemplate                          *template.Template
+	huobiTradeBuyOrderTemplate                   *template.Template
+	huobiTradeBuyOrderDiffCommissionUnitTemplate *template.Template
+	huobiTradeSellOrderTemplate                  *template.Template
+)
