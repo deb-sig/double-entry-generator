e8f9e55b128a17aa07d2689bc8002baad812f9ee
Zhenzhen Zhao <me@triplez.cn>
2024-09-17 14:25:38 +0800

    feat: support price range in `alipay`/`wechat` providers (#131)
    * feat: support price range in `alipay`/`wechat` providers
    
    * chore: lint code
    
    Signed-off-by: TripleZ <me@triplez.cn>
    
    * chore: fix lint
    
    * chore: fix lint
    
    Signed-off-by: TripleZ <me@triplez.cn>
    
    * fix: check error rule
    
    Signed-off-by: TripleZ <me@triplez.cn>
    
    ---------
    
    Signed-off-by: TripleZ <me@triplez.cn>

--- pkg/analyser/wechat/wechat.go
+++ pkg/analyser/wechat/wechat.go
@@ -93,14 +93,23 @@ func (w Wechat) GetAccountsAndTags(o *ir.Order, cfg *config.Config, target, prov
 		if r.Time != nil {
 			match, err = util.SplitFindTimeInterval(*r.Time, o.PayTime, match)
 			if err != nil {
-				log.Fatalf(err.Error())
+				log.Fatalf("Error in SplitFindTimeInterval: %v", err)
 			}
 		}
 		if r.TimestampRange != nil {
 			match, err = util.SplitFindTimeStampInterval(*r.TimestampRange, o.PayTime, match)
 			if err != nil {
-				log.Fatalf(err.Error())
+				log.Fatalf("Error in SplitFindTimeStampInterval: %v", err)
 			}
+		}
+		if r.MinPrice != nil && r.MaxPrice != nil && *r.MinPrice > *r.MaxPrice {
+			log.Fatalf("Error rule: minPrice > maxPrice!")
+		}
+		if r.MinPrice != nil && o.Money < *r.MinPrice {
+			match = false
+		}
+		if r.MaxPrice != nil && o.Money > *r.MaxPrice {
+			match = false
 		}
 
 		if match {
--- pkg/provider/wechat/util.go
+++ pkg/provider/wechat/util.go
@@ -50,7 +50,7 @@ func getTxType(tt string) TxType {
 		return TxTypeFamilyCard
 	} else if strings.Contains(tt, string(TxTypeSponsorCode)) {
 		return TxTypeSponsorCode
-	} else if strings.Contains(tt, string(TxTypeOther)){
+	} else if strings.Contains(tt, string(TxTypeOther)) {
 		return TxTypeOther
 	} else {
 		return TxTypeUnknown
